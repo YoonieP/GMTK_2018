81deecdfd99729e5b146c50aa3a9db4404f84023
YoonieP <34089410+YoonieP@users.noreply.github.com>
2018-09-01 19:44:50 +0200

    MaterialEmmisionScript

---
+++ Deathlogger/Assets/Scripts/ColorScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorScript : MonoBehaviour {
+
+    public Material selectMat;
+    bool emmision;
+    // Use this for initialization
+    void Start () {
+        selectMat.EnableKeyword("_EMISSION");
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (emmision)
+        {
+            selectMat.EnableKeyword("_EMISSION");
+        }
+        else
+        {
+            selectMat.DisableKeyword("_EMISSION");
+        }
+        emmision = !emmision;
+
+
+    }
+}
