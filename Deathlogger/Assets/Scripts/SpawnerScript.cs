c6d2725bd9a458d7f2af5a0e3c652a39adeedc3a
micromic5 <mike.amacker@gmx.ch>
2018-09-01 16:21:25 +0200

    small cleanup Spider prefab

---
+++ Deathlogger/Assets/Scripts/SpawnerScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerScript : MonoBehaviour {
+    public int sameTimeActiveEnemys = 2;
+    public int startingArraySize = 20;
+    private GameObject[] enemyArray;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
