f269d61346814c0df6e11e4f95323f3450c7c4ef
micromic5 <mike.amacker@gmx.ch>
2018-09-01 03:59:20 +0200

    shoot and destroy enemys

---
+++ Deathlogger/Assets/Scripts/ShootCollisionScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootCollisionScript : MonoBehaviour {
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "enemy")
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+}
