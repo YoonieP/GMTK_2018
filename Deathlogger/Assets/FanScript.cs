a6dfbe25c443176b615672fb6a19987dcba92efa
YoonieP <34089410+YoonieP@users.noreply.github.com>
2018-09-02 14:37:09 +0200

    Add Fan Fix Camera MAIN

---
+++ Deathlogger/Assets/FanScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FanScript : MonoBehaviour {
+    float x;
+    public GameObject fan;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        x += Time.deltaTime * 1.5f;
+        fan.transform.Rotate(new Vector3(0, x, 0));
+
+    }
+}
