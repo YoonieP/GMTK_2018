ab8cb286edefd96a4149e9ea5db9bb758b0e23b8
YoonieP <34089410+YoonieP@users.noreply.github.com>
2018-09-02 20:32:42 +0200

    BugFix

--- Deathlogger/Assets/Scripts/LevelLoad.cs
+++ Deathlogger/Assets/Scripts/LevelLoad.cs
@@ -12,9 +12,9 @@ public class LevelLoad : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if(script.points > 5 && SceneManager.GetActiveScene().buildIndex == 2)
+		if(script.points > 10 && SceneManager.GetActiveScene().buildIndex == 2)
         {
-            new WaitForSeconds(2);
+            new WaitForSeconds(3);
             SceneManager.LoadScene(1);
         }
 	}
